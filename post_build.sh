823e4a0530a6a84507619d8c86c495570c6dc83a
Abimereki Muzaale <apollo@Athena.local>
2024-06-13 14:33:59 -0400

    webApp embedded in jupyter book

---
+++ post_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the source and destination paths
+SOURCE_DIR="thesis/webApp"
+DEST_DIR="thesis/_build/html/webApp"
+
+# Remove the old destination directory if it exists
+rm -rf "$DEST_DIR"
+
+# Create the destination directory
+mkdir -p "$DEST_DIR"
+
+# Copy the webApp folder to the destination directory
+cp -r "$SOURCE_DIR/"* "$DEST_DIR/"
+
+echo "webApp folder moved to _build/html/webApp"
